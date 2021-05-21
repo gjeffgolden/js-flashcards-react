70f889fdcf8fbbd0f8bdf46a5d7ca190ab03155d
gjeffgolden <gjgolden08@gmail.com>
2021-05-21 17:59:38 -0600

    Adding HTML cards

---
+++ src/assets/HTMLElements.js
@@ -0,0 +1,10 @@
+const HTMLElements = [
+    {
+        cardType: "HTML",
+        term: "<section>",
+        definition: "General content grouping that doesn't fit more specific tags.",
+        example: "Do not nest inside another section (top-level element)."
+    },
+]
+
+export default HTMLElements;
