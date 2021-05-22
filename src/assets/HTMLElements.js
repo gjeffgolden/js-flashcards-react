553d4f92fb4db416e8bc8738af32d4e2301492dc
gjeffgolden <gjgolden08@gmail.com>
2021-05-21 18:03:09 -0600

    Adding HTML cards and logic.

--- src/assets/HTMLElements.js
+++ src/assets/HTMLElements.js
@@ -5,6 +5,24 @@ const HTMLElements = [
         definition: "General content grouping that doesn't fit more specific tags.",
         example: "Do not nest inside another section (top-level element)."
     },
+    {
+        cardType: "HTML",
+        term: "<blockquote>",
+        definition: "Quote that requires citation.",
+        example: "Includes indentation."
+    },
+    {
+        cardType: "HTML",
+        term: "<article>",
+        definition: "Independent, unique, reusable content.",
+        example: "Cards, blog content, bio, etc."
+    },
+    {
+        cardType: "HTML",
+        term: "<nav>",
+        definition: "Navigational elements.",
+        example: "Should have links as direct children."
+    },
 ]
 
 export default HTMLElements;
