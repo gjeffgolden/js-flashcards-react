afb03b7ddd7cfec1fe2c49660bbe088f4e11ed84
gjeffgolden <gjgolden08@gmail.com>
2021-05-24 16:36:24 -0600

    Building out Method array of objects.

--- src/assets/PrototypeMethods.js
+++ src/assets/PrototypeMethods.js
@@ -11,6 +11,18 @@ const prototypeMethods = [
         definition: "Array Method: Slices out section of array based on index and returns new array (non-destructive).",
         example: "[1, 2, 3].slice(0,2) => [1, 2]"
     },
+    {
+        cardType: "Method",
+        term: ".concat()",
+        definition: "Array Method: Merges two or more arrays and returns new array (non-destructive).",
+        example: "[1, 2, 3].concat([4,5])) => [1, 2, 3, 4, 5]"
+    },
+    {
+        cardType: "Method",
+        term: ".copyWithin()",
+        definition: "Array Method: Copies part of an array and pastes it to another location in same array without modifying its length (destructive). Three params: (target, start index, end index).",
+        example: "[1, 2, 3].copyWithin(0, 1, 2)) => [2, 2, 3]"
+    },
 ]
 
 export default prototypeMethods;
